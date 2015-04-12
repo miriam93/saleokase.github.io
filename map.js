ed0aae398790562fc2176ab3f417fd601e06d9f8
ergl <ergl@users.noreply.github.com>
2015-04-12 23:30:50 +0200

    Integrated map canvas Overlayed main menu. Hides the map navigation widget, but haven't found a good workaraound for that particular issue.

---
+++ map.js
@@ -0,0 +1,24 @@
+// () -> ()
+function initialize() {
+  var mapOptions = {
+    center: { lat: 40.417, lng: -3.702}, // Puerta del Sol, Madrid
+    zoom: 10
+  };
+  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+  
+  // Placeholder for showcase purpose only
+  google.maps.event.addListener(map, 'click', function(event) {
+    toggleContext();
+  });
+  
+  // gets user coordinates on right click
+  google.maps.event.addListener(map, 'rightclick', function(event) {
+    var latitude = event.latLng.lat();
+    var longitude = event.latLng.lng();
+    console.log(latitude + ', ' + longitude);
+  
+    map.panTo(new google.maps.LatLng(latitude, longitude));
+});
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
