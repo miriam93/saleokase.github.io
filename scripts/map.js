2e700b7742d21bbcf30d81083f1a63b59f5286fe
ergl <ergl@users.noreply.github.com>
2015-04-16 00:14:24 +0200

    Moved all js files to scripts/ subfolder

--- scripts/map.js
+++ scripts/map.js
@@ -7,18 +7,13 @@ function initialize() {
   };
   var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
   
-  // Placeholder for showcase purpose only
-  google.maps.event.addListener(map, 'click', function(event) {
-    toggleEventPane();
-  });
-  
   // gets user coordinates on right click
   google.maps.event.addListener(map, 'rightclick', function(event) {
     var latitude = event.latLng.lat();
     var longitude = event.latLng.lng();
     console.log(latitude + ', ' + longitude);
-  
     map.panTo(new google.maps.LatLng(latitude, longitude));
+    toggleEventPane();
 });
 }
 
