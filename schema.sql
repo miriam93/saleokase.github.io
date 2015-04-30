4d64f3a4c5ae1acb291a473187b5d6fcf85f5737
ergl <ergl@users.noreply.github.com>
2015-04-30 21:10:35 +0200

    Added events_comments table, added todo's

--- schema.sql
+++ schema.sql
@@ -7,6 +7,7 @@ create table users (
 	username varchar(50) not null,
 	-- TODO: Change after we truly know the hash length - Using SHA(256) for now
 	password varchar(128) not null
+	-- TODO: Image blob
 );
 
 create table users_users (
@@ -24,22 +25,41 @@ create table events (
 	eventsId uuid primary key,
 	title varchar(50) not null,
 	description varchar not null,
+	-- TODO: Assign to [deleted] on author deleting his account
 	author uuid references users(usersId),
 	attending int not null,
 	initialDate date not null,
 	finalDate date,
 	coordinatesId uuid references coordinates (coordinatesId)
+	-- TODO: Payments, fee?
+);
+
+create table users_events (
+	usersId uuid references users(usersId),
+	eventsId uuid references events(eventsId)
 );
 
 create table comments (
 	commentsId uuid primary key,
-	eventId uuid references events(eventsId),
 	-- ID of parent comment - if it's the same as self, there are no parents
 	parentId uuid references comments(commentsId) on delete cascade,
 	content varchar not null,
+	-- TODO: Assign to [deleted] on author deleting his account
 	author uuid references users(usersId),
 	positiveVotes int,
 	negativeVotes int
+);
+
+create table events_comments (
+	eventId uuid references events(eventsId),
+	commentsId uuid references comments(commentsId)
+	-- TODO: Deleting all comments once event is deleted?
+);
+
+-- TODO: Is this table really necessary?
+create table users_comments (
+	author uuid references users(usersId),
+	commentsId uuid references comments(commentsId)
 );
 
 create table reports (
@@ -57,3 +77,6 @@ create table comments_reports (
 	reportsId uuid references reports(reportsId) on delete cascade,
 	commentsId uuid references comments(commentsId) on delete cascade
 );
+
+create index idxusername on users(username);
+create index idxemail on users(email);
